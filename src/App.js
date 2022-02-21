c042ee0fe6a601d030177bd729d862794db73794
Nooruddin <nooruddin.rahmani100@gmail.com>
2022-02-21 21:33:37 +0430

    added app component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Tab from "./components/Tab";
+import Head from "./components/Head";
+
+function App() {
+  return (
+    <div>
+      <Head />
+      <Tab />
+    </div>
+  );
+}
+
+export default App;
