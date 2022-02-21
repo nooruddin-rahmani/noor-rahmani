5ad2a9940a2019bbbbb58db239a747f51b11e52a
Nooruddin <nooruddin.rahmani100@gmail.com>
2022-02-21 21:32:51 +0430

    initialized project

---
+++ README.md
@@ -0,0 +1,17 @@
+# Simple portfolio
+
+Build with react & Tailwindcss
+
+## Script you can run after cloning the project
+
+run:
+
+### `npm start`
+
+to run the project
+
+run:
+
+### `npm run build`
+
+to build the app for production to the `build` folder.\
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      backgroundColor: {
+        grey: "#1d1d1d",
+        blackey: "rgba(0,0,0,0.19)",
+      },
+      textColor: {
+        grey: "#ECECEC",
+      },
+    },
+  },
+  plugins: [],
+};
